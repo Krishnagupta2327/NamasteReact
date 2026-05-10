347fdbe38970543a4c055fc28809aaeed1d2a7b3
Krishna Gupta <krishna.kietian@gmail.com>
2026-05-10 12:40:38 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const heading = React.createElement("h1",{
-    id:"heading",
-    xyz:"xyz"
-},"Hello world!!");
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading);
